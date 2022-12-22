b868277f66c03f0eea43374765f78652a39caca4
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2022-12-22 17:29:16 +0100

    Working on consuming co-author xml api

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc
+    ( fetchCoAuthorInfo,
+      parseCoAuthorXml,
+      CoAuthorInfo(..),
+      PersonInfo(..)
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.ByteString.Char8 as B8
+import Network.HTTP.Simple
+
+data PersonInfo = PersonInfo {
+  name :: String,
+  urlpt :: String,
+  pid :: String
+} deriving (Eq, Show)
+
+data CoAuthorInfo = CoAuthorInfo {
+  person :: PersonInfo,
+  coAuthors :: [PersonInfo]
+} deriving (Eq, Show)
+
+fetchCoAuthorInfo :: IO ()
+fetchCoAuthorInfo = do
+  res <- httpBS "https://dblp.org/rec/pers/m/McCallum:Andrew/xc"
+  B8.putStrLn $ getResponseBody res
+  pure ()
+
+parseCoAuthorXml :: String -> CoAuthorInfo
+parseCoAuthorXml txt = undefined
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,32 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+import Text.RawString.QQ
+
+import Test.Hspec
+
+import Lib
+
+coAuthorInfoXmlText :: String
+coAuthorInfoXmlText = [r|
+<?xml version="1.0" encoding="US-ASCII"?>
+<coauthors author="Andrew McCallum" urlpt="m/McCallum:Andrew" pid="m/AndrewMcCallum" n="410">
+<author urlpt="a/Abadi:Mart=iacute=n" pid="a/MartinAbadi" count="2">Mart&#237;n Abadi</author>
+<author urlpt="a/Abdelaziz:Ibrahim" pid="153/1958" count="1">Ibrahim Abdelaziz</author>
+</coauthor>
+|]
+
+coAuthorInfo :: CoAuthorInfo
+coAuthorInfo = CoAuthorInfo {
+  person = PersonInfo { name = "", urlpt = "", pid = "" },
+  coAuthors = [
+      PersonInfo { name = "", urlpt = "", pid = "" },
+      PersonInfo { name = "", urlpt = "", pid = "" }
+    ]
+}
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "parseCoAuthorXml" $ do
+    it "should parse co-author xml" $ do
+      parseCoAuthorXml coAuthorInfoXmlText `shouldBe` coAuthorInfo
+
